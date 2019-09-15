cc00224b1121e35d91b722b0d75e54fafbf6fe13
Sergey Ermakov <ermakov42@mail.ru>
2019-09-15 17:11:05 +0700

    fixed bug

--- src/main/java/main/TaskController.java
+++ src/main/java/main/TaskController.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import main.model.Task;
-
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
