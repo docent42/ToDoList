3bbce3c922f16a23ffe42d1a7ad095273946d5ad
Sergey Ermakov <ermakov42@mail.ru>
2019-09-16 00:48:31 +0700

    Final edition. Fixed bug @Service

--- src/main/java/main/service/impl/TaskServiceImpl.java
+++ src/main/java/main/service/impl/TaskServiceImpl.java
@@ -4,11 +4,13 @@ import main.model.Task;
 import main.repository.TaskRepository;
 import main.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TaskServiceImpl implements TaskService
 {
     @Autowired
